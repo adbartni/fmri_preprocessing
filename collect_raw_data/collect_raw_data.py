ef4a78f7372598f90e1ce88524c90843c054cfa3
Alexander Bartnik <abartnik@donut.bnac.kaleidahealth.org>
2019-05-21 13:43:21 -0400

    Initiating repo

---
+++ collect_raw_data/collect_raw_data.py
@@ -0,0 +1,33 @@
+import os
+import subprocess
+from shutil import copyfile
+
+
+def init_fmri_subject_dir(subjectID, path_fmri, path_HCP):
+
+    ## Create subject directory if it does not exist
+    if not os.path.isdir(path_fmri  + subjectID):
+        os.mkdir(path_fmri + subjectID)
+
+    ## Copy brainmask into fmri subject directory
+    copyfile(path_HCP + "/T1w_acpc_dc_restore_brain.nii.gz", path_fmri + subjectID + "/brainmask.nii.gz")
+
+    ## Copy QSM data to fmri subject directory
+    strippedID = subjectID[2:]
+    path_QSM = os.path.join("/shared/studies/nonregulated/qsm_repo/data/", strippedID + "/recon/")
+    for qsm_file in ["magnitude_combined.nii.gz", "phase_combined.nii.gz"]:
+        copyfile(os.path.join(path_QSM, qsm_file), os.path.join(path_fmri, strippedID + qsm_file))
+
+
+def download_raw_fmri_data(subjectID, path_fmri):
+
+    BBS_info = "bluesky_info -i BBS -o " + subjectID + " | grep 'FMRI RESTING STATE' | cut -d"|" -f2 | sort -n | head -n 1"
+    try:
+        fmriID = subprocess.check_output(BBS_info, shell=True)
+        fmriID = ''.join(fmriID)
+
+        os.system("bluesky_retriever -i BBS -o " + os.path.join(path_fmri, subjectID, fmriID))
+        # os.rename(os.path.join(path_fmri, subjectID, fmriID), os.path.join(path_fmri, subjectID, "rawfunc.nii.gz"))
+
+    except:
+        print("{}: Something wrong".format(subjectID))
---
+++ main.py
@@ -0,0 +1,55 @@
+#!/usr/bin/ipython
+
+import sys
+import re
+from collect_raw_data.collect_raw_data import init_fmri_subject_dir, download_raw_fmri_data
+from preprocessing.Preprocessing import Preprocessing
+
+
+class PreprocessingPipeline:
+
+    def __init__(self, input_subject_list):
+        self.input_subject_list = input_subject_list
+
+    def pipeline(self):
+
+        subject_list = open(self.input_subject_list)
+        for subjectID in subject_list:
+            if re.search('[0-9]', subjectID):
+                subjectID = check_prefix(subjectID)
+                subjectID = subjectID.strip()
+            else:
+                continue
+
+            print(subjectID)
+            init_fmri_subject_dir(subjectID,
+                    "/shared/studies/nonregulated/connectome/fmri/subjects/",
+                    "/shared/studies/nonregulated/connectome/Subjects/" + subjectID + "/T1w/")
+
+            processing = Preprocessing(subjectID)
+            processing.remove_first_two_volumes()
+            # processing.slicetime_correction()
+            # processing.motion_correction()
+
+            break
+
+
+def check_prefix(subject):
+
+    if "EX" not in subject:
+        subject = "EX" + subject
+
+    return subject
+
+def create_threads(full_subject_list, num_threads):
+
+    for i in range(0, len(full_subject_list), num_threads):
+        yield(full_subject_list[i:i + num_threads])
+
+
+if __name__ == "__main__":
+
+    full_subject_list = sys.argv[1]
+
+    preproc = PreprocessingPipeline(full_subject_list)
+    preproc.pipeline()
